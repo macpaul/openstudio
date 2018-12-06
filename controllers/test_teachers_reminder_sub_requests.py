45e769fd406750b624094e28ff805fcffc72cb00
Magnus <ferric.danavis@gmail.com>
2018-12-06 13:24:01 +0100

    New page to test phase2 sending reminder emails

--- controllers/test_teachers_reminder_sub_requests.py
+++ controllers/test_teachers_reminder_sub_requests.py
@@ -4,7 +4,7 @@ from openstudio.os_scheduler_tasks import OsSchedulerTasks
 
 
 @auth.requires(auth.user_id == 1)
-def index():
+def test_teachers_reminder_sub_requests():
     """
     Function to expose class & method used by scheduler task
     to create monthly invoices
@@ -13,19 +13,7 @@ def index():
          and not auth.has_membership(group_id='Admins') ):
         redirect(URL('default', 'user', args=['not_authorized']))
 
-    db.sys_email_templates.insert(
-            Name='teacher_reminder_sub_request',
-            Title='Reminder sub request',
-            TemplateContent="""<p>Dear {teacher_name},</p>
-            <p>We would like to remind you, that there is an open {class_type} class in {class_location} on the {class_date} 
-            at {class_starttime} that needs substitution.</p>
-            <p>&nbsp;</p>
-            <p>We thank you for considering to sub this class.</p>
-            <p>&nbsp;</p>
-            <p>Best regards,</p>
-            <p>The Office Team</p>"""
 
-    )
     ost = OsSchedulerTasks()
     #
     # year = request.vars['year']
